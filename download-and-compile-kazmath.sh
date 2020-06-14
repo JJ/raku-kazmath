5dcf88954bb9296c8a624f7c194a2c65bc3dc109
JJ Merelo <jjmerelo@gmail.com>
2020-06-14 11:48:34 +0200

    Adds g++ to build tools.

--- download-and-compile-kazmath.sh
+++ download-and-compile-kazmath.sh
@@ -8,6 +8,9 @@ git clone --depth 1 https://github.com/Kazade/kazmath.git
 cd kazmath
 mkdir build
 cd build
-cmake -DBUILD_SHARED_LIBS=YES ..
+cmake -DBUILD_SHARED_LIBS=YES -DKAZMATH_BUILD_JNI_WRAPPER=OFF \
+-DKAZMATH_BUILD_GL_UTILS=OFF -DKAZMATH_BUILD_LUA_WRAPPER=OFF \
+-DKAZMATH_BUILD_TESTS=OFF
+ ..
 make
 make install
